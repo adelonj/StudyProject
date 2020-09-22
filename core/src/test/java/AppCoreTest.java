4211c623d749a5d88500ddfdd4bf07d55894252e
delonj <delonj.anna@gmail.com>
2020-09-22 19:27:27 +0300

    fist commit

---
+++ core/src/test/java/AppCoreTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppCoreTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertEquals("test", "test");
+    }
+}
---
+++ web/src/test/java/AppWebTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AppWebTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertEquals("test", "test");
+    }
+}
